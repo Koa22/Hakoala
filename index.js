1dd441022fb374f73647af7247312f26a032373d
Koalan <alan_260499@hotmail.com>
2017-10-19 17:20:20 -0500

    esta bien

---
+++ index.js
@@ -0,0 +1,26 @@
+//console.log('hola mundo')
+const lodash=require('lodash')
+
+const obj = {
+	nombre: 'alan',
+	edad: 18,
+	email: 'alan_260499@hotmail.com',
+	tw: '@koalan'
+
+}
+
+console.log(obj.edad)
+
+//function hola(){
+
+//	return 'hola mundo x2'
+//}
+
+
+
+//console.log(hola())
+
+//console.log(2 + 2)
+
+//var lista=[1,2,3,4,5]
+//console.log(lista)
